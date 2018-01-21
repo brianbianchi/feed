a6f086c5c18c1b68402ce4f80e993fae2c7aede9
brianbianchi <bianchibrianj@gmail.com>
2018-01-21 02:50:44 -0500

    modal, UI and minor logic fixes

--- feed.js
+++ feed.js
@@ -4,38 +4,58 @@
   angular.module('feedApp', ['sky', 'ngSanitize'])
   .controller('FeedController', ['FeedService', function(Feed) {
     var feed = this;
-    feed.subs = [
-    {source:'Reddit Front Page', url:'https://www.reddit.com/.rss'},
-    {source:'The Guardian', url:'https://www.theguardian.com/international/rss'},
-    //{source:'Google Trending', url:'https://trends.google.com/trends/hottrends/atom/hourly'}
+    feed.subs = [];
+    feed.popular = [
+    {title:'Reddit Front Page', url:'https://www.reddit.com/.rss'},
+    {title:'The Guardian', url:'https://www.theguardian.com/international/rss'},
+    {title:'CNN', url:'http://rss.cnn.com/rss/cnn_topstories.rss'},
+    {title:'NY Times', url:'http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml'},
+    {title:'youtube', url:'https://www.youtube.com/feeds/videos.xml?channel_id=UCBcRF18a7Qf58cCRy5xuWwQ'},
     ];
     feed.pages = [];
 
-    feed.addFeed = function() {
-      feed.subs.push({source:feed.feedUrl, url:feed.feedUrl});
+    feed.addFeed = function(feedurl) {
+      feed.subs.push({title:'', url:feedurl});
       feed.feedUrl = '';
       feed.displayFeed();
+      angular.forEach(feed.popular, function(popPage) {
+        angular.forEach(feed.subs, function(subPage) {
+          if (popPage.url == subPage.url) {
+            feed.removeFeed(popPage, 'popular');
+          }
+        });
+      });
     };
 
-    feed.removeFeed = function(removeSub) {
-      var oldSubs = feed.subs;
-      feed.subs = [];
-      angular.forEach(oldSubs, function(page) {
-        if (page != removeSub) feed.subs.push(page);
-      });
-      feed.displayFeed();
+    feed.removeFeed = function(removePage, arr) {
+      if (arr == 'subs') {
+        var oldArr = feed.subs;
+        feed.subs = [];
+        angular.forEach(oldArr, function(page) {
+          if (page != removePage) feed.subs.push(page);
+        });
+        feed.displayFeed();
+      }
+      else if (arr == 'popular') {
+        var oldArr = feed.popular;
+        feed.popular = [];
+        angular.forEach(oldArr, function(page) {
+          if (page.url != removePage.url) feed.popular.push(page);
+        });
+      }
     };
 
     feed.displayFeed = function() {
       feed.pages = [];
       angular.forEach(feed.subs, function(key, val) {
-        console.log("key="+ key +", val="+ val +", url="+ key.url);
         Feed.parseFeed(key.url)
         .then(function (res) {
           feed.pages.push(res);
+          key.title = res.feed.title;
         });
-        console.log(feed.pages);
       })
+      console.log(feed.pages);
+      console.log(feed.subs);
     }
 
     feed.displayFeed();
